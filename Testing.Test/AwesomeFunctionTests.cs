eb998076d158de686695c98f00837302895a583e
Simon Timms <stimms@gmail.com>
2017-08-25 09:58:39 -0600

    Updating packages

--- Testing.Test/AwesomeFunctionTests.cs
+++ Testing.Test/AwesomeFunctionTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,7 +10,7 @@ namespace Testing.Test
     {
         [Fact]
         async Task Is_pretty_awesome()
-        { 
+        {
             await AwesomeFunction.Run(new System.Net.Http.HttpRequestMessage(), null);
         }
     }
